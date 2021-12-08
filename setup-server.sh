427a1167e0c4a88a8dbb8e1721558f47c7c7679a
Agentspades <74514996+Agentspades@users.noreply.github.com>
2021-12-08 12:08:19 +1000

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -1,4 +1,4 @@
 echo Starting server
 python3 ./app/manage.py migrate
 python3 ./app/manage.py createsuperuser
-python3 ./app/manage.py runserver 0.0.0.0:80
+python3 ./app/manage.py runserver 0.0.0.0:80 &
